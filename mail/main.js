4b7998fbbed8399df47eef4cb767b317f1e36785
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-02 15:36:21 +0200

    [UPD] Working on the research logic

--- mail/main.js
+++ mail/main.js
@@ -3,5 +3,21 @@
 const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", function () {
   const inputMail = document.getElementById("mail-input").value;
-  console.log(inputMail);
+  //console.log(inputMail);
 });
+
+const mailAddresses = [
+  "ale.barat@gmail.com",
+  "alebarat@gmail.com",
+  "ab@gmail.com",
+  "aleb@gmail.com",
+  "alessandrobarate@gmail.com",
+  "alessandrob@gmail.com",
+  "abarate@gmail.com",
+];
+
+console.log(mailAddresses);
+
+if (mailAddresses === inputMail) {
+  console.log("Mail presente");
+}
