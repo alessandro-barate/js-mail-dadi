678fb8c2bf072d2aef83e7b216ed622fd0b93a30
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-02 16:15:19 +0200

    [UPD] Mail exercise done

--- mail/main.js
+++ mail/main.js
@@ -16,9 +16,13 @@ console.log(mailAddresses);
 
 searchButton.addEventListener("click", function () {
   const inputMail = document.getElementById("mail-input").value;
+  let searchResult = document.getElementById("search-result");
+  let currentText = searchResult.innerHTML;
   for (let index = 0; index < mailAddresses.length; index++) {
     if (inputMail == mailAddresses[index]) {
-      console.log("Mail presente");
+      console.log("Mail trovata!");
+      const successSearch = "La tua mail è nella lista";
+      searchResult.innerHTML = successSearch;
     }
   }
 });
