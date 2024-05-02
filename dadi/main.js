34fd0f87df93528d35b0e88fe22999a025c4b6ce
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-02 18:27:33 +0200

    [UPD] Dice game - implementing the logic

--- dadi/main.js
+++ dadi/main.js
@@ -4,3 +4,12 @@ const userNumber = Math.floor(Math.random() * 6) + 1;
 console.log(userNumber);
 const pcNumber = Math.floor(Math.random() * 6) + 1;
 console.log(pcNumber);
+
+const numbersList = [1, 2, 3, 4, 5, 6];
+console.log(numbersList);
+
+for (let counter = 0; counter < numbersList.length; counter++) {
+  if (userNumber == numbersList[counter] && pcNumber == numbersList[counter]) {
+    console.log("Pari");
+  }
+}
